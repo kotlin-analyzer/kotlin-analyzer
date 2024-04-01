7deb500b99bcc9d7f094fa6ffa498387f2e47c85
Ikechukwu Eze <92305839+ikezedev@users.noreply.github.com>
2024-04-01 18:09:39 +0200

    scaffold ast

--- crates/ast/src/lib.rs
+++ crates/ast/src/lib.rs
@@ -1,2 +1,33 @@
 //! source: https://kotlinlang.org/spec/syntax-and-grammar.html#syntax-grammar
-pub struct Ast;
+
+mod nodes;
+
+use nodes::{
+    FileAnnotation, ImportList, Nl, PackageHeader, ShebangLine, Statement, TopLevelObject,
+};
+use std::ops::Range;
+
+pub type Span = Range<usize>;
+
+pub enum Ast {
+    File(KotlinFile),
+    Script(KotlinScript),
+}
+
+pub struct KotlinFile {
+    shebang_line: Option<ShebangLine>,
+    lines_after_shebang: Vec<Nl>,
+    file_annotations: Vec<FileAnnotation>,
+    package_header: PackageHeader,
+    import_list: ImportList,
+    top_level_objects: Vec<TopLevelObject>,
+}
+
+pub struct KotlinScript {
+    shebang_line: Option<ShebangLine>,
+    lines_after_shebang: Vec<Nl>,
+    file_annotations: Vec<FileAnnotation>,
+    package_header: PackageHeader,
+    import_list: ImportList,
+    statements: Vec<Statement>,
+}
---
+++ crates/ast/src/nodes/newline.rs
@@ -0,0 +1,6 @@
+use crate::Span;
+
+pub enum Nl {
+    Cr(Span),
+    Lf(Span),
+}
