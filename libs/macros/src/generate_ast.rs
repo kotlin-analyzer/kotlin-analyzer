60672f7872fe6029e229f5b9d0446dbe0d0aaa15
Ikechukwu Eze <92305839+ikezedev@users.noreply.github.com>
2024-05-27 20:50:52 +0200

    progress

--- libs/macros/src/generate_ast.rs
+++ libs/macros/src/generate_ast.rs
@@ -8,6 +8,104 @@ use tokens::resolve_token;
 
 use crate::parse::{Field, GenAst, ParseEntry, TopLevelParseEntry};
 
+enum SimpleName {
+    Token(String),
+    Ident(String),
+}
+
+impl SimpleName {
+    fn name(&self) -> &str {
+        match self {
+            SimpleName::Token(name) => name,
+            SimpleName::Ident(name) => name,
+        }
+    }
+
+    fn type_name(&self) -> String {
+        match self {
+            SimpleName::Token(_) => "SyntaxNode".into(),
+            SimpleName::Ident(id) => id.to_string().to_pascal_case(),
+        }
+    }
+
+    fn cast_closure(&self) -> TokenStream {
+        let ty = self.type_name();
+        match self {
+            SimpleName::Token(_) => quote!(::syntax::cast_syntax_kind(::syntax::SyntaxKind::#ty)),
+            SimpleName::Ident(_) => quote!(#ty::cast),
+        }
+    }
+}
+
+struct Single {
+    method: Option<Ident>,
+    iter_fn: Option<Ident>,
+    return_type: Option<TokenStream>,
+    cast: Option<TokenStream>,
+}
+
+enum GenInterface {
+    Single {
+        method: Option<Ident>,
+        iter_fn: Ident,
+        return_type: Option<TokenStream>,
+        cast: Option<TokenStream>,
+    },
+    Many(Vec<GenInterface>),
+    Enum {
+        variants: Vec<Ident>,
+    },
+}
+
+impl ParseEntry {
+    /// gets the best name of the entry. Usually the name of token or identifier in simple cases
+    /// but for multiple entries will try to find an identifier or default to the first token name found
+    fn simple_name(&self) -> Option<SimpleName> {
+        let mut tmp = self.identifiers().into_iter();
+        match tmp.next() {
+            a @ Some(SimpleName::Token(_)) => tmp.find(|s| matches!(s, SimpleName::Ident(_))).or(a),
+            rest => rest,
+        }
+    }
+
+    fn identifiers(&self) -> Vec<SimpleName> {
+        match self {
+            ParseEntry::CharLit(ch) => resolve_token(&ch.value().to_string())
+                .map(|t| vec![SimpleName::Token(format!("{:?}", t))])
+                .unwrap_or_default(),
+            ParseEntry::StrLit(st) => resolve_token(&st.value().to_string())
+                .map(|t| vec![SimpleName::Token(format!("{:?}", t))])
+                .unwrap_or_default(),
+            ParseEntry::Ident(id) => {
+                if id.to_string().starts_with(char::is_lowercase) {
+                    vec![SimpleName::Ident(id.to_string())]
+                } else {
+                    vec![SimpleName::Token(id.to_string())]
+                }
+            }
+            ParseEntry::Repeated(entries)
+            | ParseEntry::Choice(entries)
+            | ParseEntry::Group(entries)
+            | ParseEntry::Optional(entries) => {
+                entries.iter().flat_map(|e| e.identifiers()).collect()
+            }
+        }
+    }
+
+    fn method_name(&self) -> Option<String> {
+        self.simple_name().map(|name| match name {
+            SimpleName::Token(n) => format!("get_{}", n.to_snake_case()),
+            SimpleName::Ident(n) => {
+                format!("{}", n.to_snake_case())
+            }
+        })
+    }
+
+    fn type_name(&self) -> Option<String> {
+        self.simple_name().map(|name| name.type_name())
+    }
+}
+
 pub fn generate_ast(ast: GenAst) -> TokenStream {
     let entries = ast.entries.into_iter().map(gen_top_level);
     quote! {
@@ -22,10 +120,29 @@ fn gen_top_level(top: TopLevelParseEntry) -> TokenStream {
         .iter()
         .map(|entry| gen_parse_entry(entry, &top.field.name, 0, RetType::Optional));
     let field = gen_field(&top.field);
+    let syntax_name = format_ident!("{}", top.field.name.to_string().to_screaming_snake_case());
     quote! {
         #field
         impl #typename {
             #(#entries)*
+        }
+    };
+    quote! {
+        #[derive(PartialEq, Eq, Hash, Clone)]
+        #[repr(transparent)]
+        // todo add doc
+        pub struct #typename(::syntax::SyntaxNode);
+
+        impl #typename {
+            use ::syntax::{ SyntaxNode, SyntaxKind };
+            #[allow(unused)]
+            pub fn cast(node: SyntaxNode) -> Option<Self> {
+                if node.kind() == SyntaxKind::#syntax_name {
+                    Some(Self(node))
+                } else {
+                    None
+                }
+            }
         }
     }
 }
@@ -34,6 +151,65 @@ fn gen_top_level(top: TopLevelParseEntry) -> TokenStream {
 enum RetType {
     Optional,
     Many,
+}
+
+impl RetType {
+    fn iter_fn(&self) -> Ident {
+        match self {
+            RetType::Optional => format_ident!("find_map"),
+            RetType::Many => format_ident!("filter_map"),
+        }
+    }
+
+    fn method_name(&self, entry: &ParseEntry) -> Option<Ident> {
+        let name = entry.method_name()?;
+        let ident = match self {
+            RetType::Optional => format_ident!("{}", name),
+            RetType::Many => format_ident!("{}", name.to_plural()),
+        };
+        Some(ident)
+    }
+
+    fn return_type(&self, entry: &ParseEntry) -> Option<TokenStream> {
+        let type_name = format_ident!("{}", entry.type_name()?);
+        let ident = match self {
+            RetType::Optional => quote!(Option<#type_name>),
+            RetType::Many => quote!(impl Iterator<Item = #type_name> + '_),
+        };
+        Some(ident)
+    }
+}
+
+fn process_entry(entry: &ParseEntry, ret_type: RetType) -> GenInterface {
+    match entry {
+        ParseEntry::CharLit(_) | ParseEntry::StrLit(_) | ParseEntry::Ident(_) => {
+            GenInterface::Single {
+                method: ret_type.method_name(entry),
+                iter_fn: ret_type.iter_fn(),
+                return_type: ret_type.return_type(entry),
+                cast: entry.simple_name().map(|s| s.cast_closure()),
+            }
+        }
+        ParseEntry::Optional(entries) => {
+            GenInterface::Many(entries.iter().map(|e| process_entry(e, ret_type)).collect())
+        }
+        ParseEntry::Repeated(entries) => GenInterface::Many(
+            entries
+                .iter()
+                .map(|e| process_entry(e, RetType::Many))
+                .collect(),
+        ),
+        ParseEntry::Choice(entries) => {
+            let variants = entries
+                .iter()
+                .flat_map(|e| e.type_name().map(|id| format_ident!("{}", id)))
+                .collect();
+            GenInterface::Enum { variants }
+        }
+        ParseEntry::Group(entries) => {
+            GenInterface::Many(entries.iter().map(|e| process_entry(e, ret_type)).collect())
+        }
+    }
 }
 
 fn gen_parse_entry(
