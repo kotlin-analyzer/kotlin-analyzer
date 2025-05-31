7ccc026c5f808bba1c12b78c7b2cfac4eadd4254
Ikechukwu Eze <92305839+ikezedev@users.noreply.github.com>
2025-05-31 22:31:04 +0200

    remove unnecessary comments

--- crates/parser/build/main.rs
+++ crates/parser/build/main.rs
@@ -10,14 +10,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn gen_grammar_for_file(file_name: &str, additional_args: &[&str]) -> Result<(), Box<dyn Error>> {
-    let input = env::current_dir()
-        .unwrap()
-        .join("kotlin_spec/grammar/src/main/antlr");
+    let input = env::current_dir()?.join("kotlin_spec/grammar/src/main/antlr");
     let antlr_path = env::current_dir()?.join("build/generator/antlr4-4.8-2-SNAPSHOT-complete.jar");
-    // println!("Using ANTLR jar at: {}", antlr_path.display());
-    // let antlr_path = "/Users/ikechukwu.eze@cognitedata.com/Development/kotlin-analyzer/crates/parser/build/generator/antlr4-4.8-2-SNAPSHOT-complete.jar";
     let out_dir = env::var_os("OUT_DIR").ok_or_else(|| "OUT_DIR does not exist as env variable")?;
-    // let out_dir = "../../../../../generated";
 
     Command::new("java")
         .current_dir(input)
@@ -29,8 +24,7 @@ fn gen_grammar_for_file(file_name: &str, additional_args: &[&str]) -> Result<(),
         .arg(out_dir)
         .arg(&file_name)
         .args(additional_args)
-        .spawn()
-        .expect("antlr tool failed to start")
+        .spawn()?
         .wait_with_output()?;
 
     Ok(())
