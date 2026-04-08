0421f251ac4ae50322a2a264a77c596a2879592d
Ikechukwu Eze <92305839+ikezedev@users.noreply.github.com>
2026-04-08 23:56:11 +0200

    modifiers

--- crates/parser/src/grammar2.rs
+++ crates/parser/src/grammar2.rs
@@ -1,18 +1,7 @@
-mod identifiers;
-// #[cfg(test)]
-// mod identifiers_test;
-
 pub(crate) mod annotations;
-// #[cfg(test)]
-// mod annotations_test;
-
+mod identifiers;
+pub(crate) mod modifiers;
 pub(crate) mod types;
-// #[cfg(test)]
-// mod types_test;
-
-// pub(crate) mod modifiers;
-// #[cfg(test)]
-// mod modifiers_test;
 
 // pub(crate) mod utils;
 
